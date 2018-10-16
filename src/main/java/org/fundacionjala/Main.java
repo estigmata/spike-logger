d7413c2fada7e3e842f5136f87d9018e4ef99de6
estigmata <nestor.otondo@fundacion-jala.org>
2018-10-16 01:10:51 -0400

    Create logger

---
+++ src/main/java/org/fundacionjala/Main.java
@@ -0,0 +1,30 @@
+package org.fundacionjala;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class Main {
+  private static final Logger log = Logger.getLogger(Main.class.getName());
+
+  public static void main(String[] args) {
+    FileHandler file;
+    log.setLevel(Level.FINER);
+    log.fine("Begin my app.");
+    log.entering(Main.class.getName(), "Do it.");
+    System.out.println("Hello Logger");
+    try {
+      file = new FileHandler("C:/temp/my-log-file.log");
+      log.addHandler(file);
+      SimpleFormatter formatter = new SimpleFormatter();
+      file.setFormatter(formatter);
+      log.info("Hey we are going.");
+    } catch (Exception e) {
+      log.warning("File cannot be created.");
+    }
+    log.info("The app show 'Hello Logger'");
+    log.exiting(Main.class.getName(), "Do it.");
+  }
+}
